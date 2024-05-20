255ca58405915beb30598ff7d89e34011e9ef0b9
vidhathri30 <vidhathri30@gmail.com>
2024-05-21 00:46:20 +0530

    episode 6

--- src/component/Body.js
+++ src/component/Body.js
@@ -1,26 +1,62 @@
 
 import RestoCard from "./RestoCard";
-import resList from "../utils/mockdata";
-import { useState } from "react";
+//import resList from "../utils/mockdata";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body=()=>{
-    const [ListOfRestaurants,setListOfRestaurants]=useState(resList);
-    return (
-        <div className="body">
-            
-            <button className="top-restaurants" onClick={()=>{
-                
-                const filtereddata= ListOfRestaurants.filter((res)=>res.info.avgRating >4.5);
-                 setListOfRestaurants(filtereddata);
-            }}>Top Listed Restaurants</button>
-            <div className="res-container">
-              {
-                 ListOfRestaurants.map((restaurant)=>(<RestoCard key={restaurant.info.id} resData={restaurant} />))
-              }
-                
+    const [ListOfRestaurants,setListOfRestaurants]=useState([]);
+    const [ListOfFilterdRestaurants,setListOfFilterdRestaurants]=useState([]);
+    const [searchText,setsearchText]=useState(" "); 
+    useEffect(()=>{
+        fetchData();
+    },[]);
+    const fetchData= async ()=>{
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9587533&lng=77.5619827&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const json = await data.json();
+        //optional rendering
+        setListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setListOfFilterdRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+
+    }
+    //conditional rendering
+   
+    
+        return ListOfRestaurants.length===0?<Shimmer/>:(
+            <div className="body">
+                <div className="upper-body">
+                    <div className="search">
+                        <input type="text" className="search-box" value={searchText} onChange={(e)=>{
+                            setsearchText(e.target.value);
+                        }}/>
+                        
+                        <button onClick={()=>{
+                            const filtered = ListOfRestaurants.filter((res)=>
+                                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+                        );
+                            setListOfFilterdRestaurants(filtered);
+                    }
+                        }>search</button>
+                    </div>
+                    <div>
+                <button className="top-restaurants" onClick={()=>{
+                    
+                    const filtereddata= ListOfRestaurants.filter((res)=>res.info.avgRating >4.1);
+                     setListOfFilterdRestaurants(filtereddata);
+                }}>Top Listed Restaurants</button></div>
+                </div>
+                <div className="res-container">
+                  {
+                     ListOfFilterdRestaurants.map((restaurant)=>(<RestoCard key={restaurant.info.id} resData={restaurant} />))
+                    
+                    }
+                  
+                    
+                </div>
             </div>
-        </div>
-        
-    );
+        );
+            
+          
+    
  }
  export default Body;
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,5 +1,8 @@
 import { LOGO_URL } from "../utils/constants";
+import {useState} from "react";
+
  const Header=()=>{
+  const [buttonchange , setbuttonchange]=useState("login");
     return (
         <div className="header">
           <div className="logo-container">
@@ -11,6 +14,9 @@ import { LOGO_URL } from "../utils/constants";
                 <li>About</li>
                 <li>Contact us</li>
                 <li>Cart</li>
+                <li><button className="btn" onClick={()=>{
+                  return buttonchange==="login"?setbuttonchange("logout"):setbuttonchange("login");
+                }}>{buttonchange}</button></li>
             </ul>
           </div>
 
